1d688d6698713666fca7b68d7c4d97cf699cb8bb
n_popov <npopov2@yandex.com>
2026-02-15 09:30:30 +0300

    Added filter for employees

--- inventory_web/employees/filters.py
+++ inventory_web/employees/filters.py
@@ -4,10 +4,11 @@ from .models import Employee
 
 class EmployeeFilter(django_filters.FilterSet):
     company = django_filters.ModelChoiceFilter(queryset=Company.objects.all())
+    name = django_filters.CharFilter(lookup_expr='icontains')
 
     class Meta:
         model = Employee
-        fields = ['company']
+        fields = ['company', 'name']
 
     def filter_self_company(self, queryset, name, value):
         if value:
